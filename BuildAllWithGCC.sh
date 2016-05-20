18d1903ba8300c7bc520191ceff83098f8a683b1
meriken <meriken.ygch.net@gmail.com>
2016-05-20 07:50:18 -0700

    Bug fixes

--- BuildAllWithGCC.sh
+++ BuildAllWithGCC.sh
@@ -15,7 +15,8 @@ cd BoostPackages/
 7z x -y boost_1_61_0.7z
 cd boost_1_61_0
 ./bootstrap.sh --with-toolset=gcc
-./b2 toolset=gcc link=static runtime-link=static -j 8
+# ./b2 toolset=gcc link=static runtime-link=static -j 8 # FreeBSD is not happy.
+./b2 toolset=gcc link=static -j 8
 cd ../../
 
 mkdir CMakeBuild/
