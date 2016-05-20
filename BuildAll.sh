8137584c7383a1a2c5ceb403b6996547516f0bad
meriken <meriken.ygch.net@gmail.com>
2016-05-19 20:30:32 -0700

    Another change for FreeBSD

--- BuildAll.sh
+++ BuildAll.sh
@@ -15,8 +15,15 @@ cd BoostPackages/
 7z x -y boost_1_61_0.7z
 cd boost_1_61_0
 ./bootstrap.sh 
-# ./b2 link=static runtime-link=static -j 8 # FreeBSD is not happy.
-./b2 link=static -j 8
+if [ -f ./b2 ]; then
+	# ./b2 link=static runtime-link=static -j 8 # FreeBSD is not happy.
+	./b2 link=static -j 8
+else
+    # bootstrap.sh cannot automatically detect clang on FreeBSD.
+	./bootstrap.sh --with-toolset=clang
+	# ./b2 toolset=clang link=static runtime-link=static -j 8 # FreeBSD is not happy.
+	./b2 toolset=clang link=static -j 8
+fi
 cd ../../
 
 mkdir CMakeBuild/
