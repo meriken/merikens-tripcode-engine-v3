c58f6fde49bf147e4cba04e58878d836fc7e4b2d
meriken <meriken@users.noreply.github.com>
2016-05-22 04:14:49 -0700

    Update BuildAllWithGCC.sh

--- BuildAllWithGCC.sh
+++ BuildAllWithGCC.sh
@@ -24,11 +24,8 @@ cd ../../
 mkdir CMakeBuild/
 cd CMakeBuild/
 cmake -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++ ../SourceFiles/
-if ! make
-then
-    cmake -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++ -DENABLE_OPENCL=OFF ../SourceFiles/
-    make
-fi
+make
+
 echo "#regex" > patterns.txt
 echo "^TEST." >> patterns.txt
 echo Testing MerikensTripcodeEngine...
